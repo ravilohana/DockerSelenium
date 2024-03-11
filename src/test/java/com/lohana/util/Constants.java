4ac7890ca88250d217dc2f8d8213f9038c086d79
ravilohana <ravi.lohana1987@gmail.com>
2024-03-11 12:57:10 +0530

    update initial commit

---
+++ src/test/java/com/lohana/util/Constants.java
@@ -0,0 +1,22 @@
+package com.lohana.util;
+
+public class Constants {
+
+    public static final String GRID_ENABLED = "selenium.grid.enable";
+    public static final String GRID_URL_FORMAT = "selenium.grid.urlFormat";
+    public static final String GRID_HUB_HOST = "selenium.grid.hubHost";
+    public static final String BROWSER_NAME = "browserName";
+    public static final String CHROME = "chrome";
+    public static final String FIREFOX = "firefox";
+    public static final String MS_EDGE = "ms-edge";
+
+    public static final String DRIVER = "driver";
+
+    public static final String FLIGHT_RESERVATION_URL = "flightReservation.url";
+    public static final String VENDOR_PORTAL_URL = "vendorPortal.url";
+
+    public static final String APP_WAITS = "explicitAppWaits";
+
+
+
+}
